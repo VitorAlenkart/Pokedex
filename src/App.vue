ae8ab1c4cb96d6d8fc978aa50d36b89606f03539
VitorAlenkart <vitoralencargithub@gmail.com200~git config --global user.name VitorAlencarDev1>
2023-06-06 14:58:39 -0300

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,81 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+
+  <div id="app">
+    <NavBar/> <br>
+
+    <section class="hero is-link is-small">
+      <div class="hero-body">
+        <p class="title">
+          Pokedex
+        </p>
+        <p class="subtitle">
+          Projeto feito por @VitorAlenkart
+        </p>
+      </div>
+    </section><br>
+
+    <input class="input is-rounded" id="inputBuscar" type="text" placeholder="Buscar pokemon..." v-model="busca"> <button v-on:click="filtrarPokemons" class="button is-primary is-rounded" id="btnBuscar">Buscar</button>
+
+    <div v-for="(poke,index) in pokemonsFiltrados" :key="poke.name" id="pokemons">
+    <Pokemon :index="index +1" :pokemon="(capitalize(poke.name))" :url="poke.url" />
+    </div>
+  </div>
+
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import axios from 'axios';
+import Pokemon from './components/DetailsPokemon.vue'
+import NavBar from './components/NavBar'
 
 export default {
   name: 'App',
+
+  data() {
+    return {
+      pokemons: [],
+      pokemonsFiltrados: [],
+      busca: ''
+    }
+  },
   components: {
-    HelloWorld
-  }
+    Pokemon,
+    NavBar
+  },
+  created () {
+    axios.get('https://pokeapi.co/api/v2/pokemon?limit=151&offset=0').then(d => {
+      
+      this.pokemons = (d.data.results);
+      this.pokemonsFiltrados = this.pokemons;
+    });
+    
+  },
+  methods: {
+    capitalize: function (value) {
+      if (!value) return ''
+      return value.charAt(0).toUpperCase() + value.slice(1)
+    },
+    filtrarPokemons: function(event){
+      if(event){
+        if(this.busca === '' || this.busca === ' '){
+          this.pokemonsFiltrados = this.pokemons;
+        }else{
+          this.pokemonsFiltrados = this.pokemons.filter(p => p.name === this.busca || p.name[0].toUpperCase() + p.name.slice(1).toLowerCase() === this.busca);
+        }
+      }
+    }
+  },
+  computed: {
+    // resultadoFiltrado: function(){
+    //   if(this.busca === '' || this.busca === ' '){
+    //     return this.pokemons
+    //   }else{
+    //     return this.pokemons.filter(p => p.name === this.busca || p.name[0].toUpperCase() + p.name.slice(1).toLowerCase() === this.busca);
+    //   }
+    // }
+  },
 }
+
 </script>
 
 <style>
@@ -23,4 +87,14 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 }
+
+#btmbuscar {
+  float: left;
+}
+
+#inputBuscar {
+  max-width: 60%;
+}
+
+
 </style>
---
+++ src/components/DetailsPokemon.vue
@@ -0,0 +1,122 @@
+<template>
+    <div id="divPrinc">
+        <div class="card">
+            <div class="card-image ">
+              <figure class="image is-centered is-120x120 ">
+                <img :src="sprite">
+              
+              </figure>
+            </div>
+            <div class="card-content">
+              <div class="media">
+                <div class="media-left">
+                  <h2>{{index}}</h2>
+                </div>
+                <div class="media-content">
+
+                  <p class="title is-4">{{ pokemon }}</p>
+                  
+                  <p class="subtitle is-6" id="p1">{{capitalize(this.type.type1) + '   '}}</p>
+                  <p class="subtitle is-6" id="p2" >{{capitalize(this.type.type2)}}</p>
+      
+                </div>
+                
+              </div>
+          
+              <div class="content">
+                <button class="button is-primary is-normal" v-on:click="changeSprite">Trocar lado!</button>
+              </div>
+            </div>
+          </div>
+        <input v-bind:hidden="url">
+    </div>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+    data () {
+        return {
+            sprite: '',
+            spriteFront: '',
+            spriteBack: '',
+            spriteSide: '',
+            type: {
+              type1: '',
+              type2: '',
+            }
+        }
+    },
+    props: {
+        index: Number,
+        pokemon: String,
+        url: String,
+    },
+    
+    methods: {
+      capitalize: function (value) {
+        if (!value) return ''
+        return value.charAt(0).toUpperCase() + value.slice(1)
+      },
+      changeSprite: function(event) {
+        if(event){
+          if(this.spriteSide === true){
+            this.spriteSide = false;
+            this.sprite = this.spriteBack;
+          }else{
+            this.sprite = this.spriteFront;
+            this.spriteSide = true;
+          }
+        }
+        
+      }
+    },
+    created () {
+        axios.get(this.url).then(d => {
+          this.spriteSide = true;
+          this.sprite = d.data.sprites.front_default;
+
+          this.spriteFront = d.data.sprites.front_default;
+          this.spriteBack = d.data.sprites.back_default;
+          
+          if(d.data.types[1] != undefined){
+            this.type = {
+              type1: d.data.types[0].type.name,
+              type2: d.data.types[1].type.name
+            }
+          }else{
+            this.type = {
+              type1: d.data.types[0].type.name
+            };
+          }
+
+        });
+    },
+}
+</script>
+
+<style scoped>
+
+.card {
+    float: left;
+    margin-left: 8%;
+    height: auto;
+    width: 370px;
+    margin-top: 2%;
+   
+}
+
+#p1 {
+  display: inline;
+}
+#p2 {
+  display: inline;
+
+}
+
+#divPrinc {
+  margin-left: 5%;
+}
+
+</style>
